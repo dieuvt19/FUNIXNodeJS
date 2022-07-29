66276b16976508a849068375dd12db0e6452b79d
Dieu Dieu <vothuydieu1903@gmail.com>
2022-07-29 18:18:55 +0700

    lay du lieu

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: "node-complete",
+  password: "Khong123",
 });
 
 module.exports = pool.promise();
