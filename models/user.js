df6440960e097ac18e713dab43000d8f1953069d
Dieu Dieu <vothuydieu1903@gmail.com>
2022-08-01 15:05:12 +0700

    them chuc nang them vao gio hang

--- models/user.js
+++ models/user.js
@@ -20,7 +20,9 @@ class User {
     // const cartProduct = this.cart.items.findByIndex((cp) => {
     //   cp._id === product._id;
     // });
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const updatedCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+    };
     const db = getDb();
     return db
       .collection("users")
