1fe2163a5c3d2924649f599006ce6eb31723d24c
Dieu Dieu <vothuydieu1903@gmail.com>
2022-08-08 15:56:34 +0700

    xoa cookie

--- app.js
+++ app.js
@@ -36,15 +36,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  User.findById("62ea88e4f229ecc76b567880")
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
