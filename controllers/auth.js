43fc06f30fc3af6b32d9a26aef5a5b6c655c9c66
Dieu Dieu <vothuydieu1903@gmail.com>
2022-08-10 13:31:49 +0700

    them kiem tra du lieu de dang nhap

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,6 +27,11 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+    },
+    validationErrors: [],
   });
 };
 
@@ -56,17 +61,30 @@ exports.postLogin = (req, res, next) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).render("auth/signup", {
-      path: "/signup",
-      pageTitle: "Signup",
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
     });
   }
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password.");
-        return res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password.",
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [],
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -79,8 +97,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash("error", "Invalid email or password.");
-          res.redirect("/login");
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            pageTitle: "Login",
+            errorMessage: "Invalid email or password.",
+            oldInput: {
+              email: email,
+              password: password,
+            },
+            validationErrors: [],
+          });
         })
         .catch((err) => {
           console.log(err);
@@ -109,7 +135,7 @@ exports.postSignup = (req, res, next) => {
       validationErrors: errors.array(),
     });
   }
-  return bcrypt
+  bcrypt
     .hash(password, 12)
     .then((hashedPassword) => {
       const user = new User({
@@ -121,12 +147,12 @@ exports.postSignup = (req, res, next) => {
     })
     .then((result) => {
       res.redirect("/login");
-      return transporter.sendMail({
-        to: email,
-        from: "shop@node-complete.com",
-        subject: "Signup succeeded!",
-        html: "<h1>You successfully signed up!</h1>",
-      });
+      // return transporter.sendMail({
+      //   to: email,
+      //   from: "shop@node-complete.com",
+      //   subject: "Signup succeeded!",
+      //   html: "<h1>You successfully signed up!</h1>",
+      // });
     })
     .catch((err) => {
       console.log(err);
