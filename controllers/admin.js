b1207953932a9acd49c2139638d688a4cfb8cb5d
Dieu Dieu <vothuydieu1903@gmail.com>
2022-08-11 14:07:49 +0700

    loc tap tin bang mimetype

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,6 +20,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  console.log(imageUrl);
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
