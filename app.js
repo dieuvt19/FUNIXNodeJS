98cc50ad394b92430fa21a34ec14d8e434d54458
Dieu Dieu <vothuydieu1903@gmail.com>
2022-09-14 21:14:01 +0700

    heroku

--- app.js
+++ app.js
@@ -105,6 +105,8 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
+      console.log("Server is running.");
+    });
   })
   .catch((err) => console.log(err));
