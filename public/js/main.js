e6b3892207138bd69aa8e4f3a4d98d6645333cda
dieuvt19 <105504666+dieuvt19@users.noreply.github.com>
2022-09-14 14:13:06 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,7 @@
-// const { JSDOM } = require("jsdom");
-// const { window } = new JSDOM("");
-// const $ = require("jquery")(window);
 
 const backdrop = document.querySelector(".backdrop");
 const sideDrawer = document.querySelector(".mobile-nav");
 const menuToggle = document.querySelector("#side-menu-toggle");
-// const btnAdd = document.querySelector(".btn-add");
 
 function backdropClickHandler() {
   backdrop.style.display = "none";
@@ -17,15 +13,11 @@ function menuToggleClickHandler() {
   sideDrawer.classList.add("open");
 }
 
-// function addInput() {
-//   const input = document.createElement("input");
-//   input.setAttribute("type", "date");
-//   input.setAttribute("name", "dateLeave");
-//   document.getElementById("input").appendChild(input);
-// }
+if (backdrop) {
+  backdrop.addEventListener("click", backdropClickHandler);
+}
 
-backdrop.addEventListener("click", backdropClickHandler);
-menuToggle.addEventListener("click", menuToggleClickHandler);
-// if (btnAdd) {
-//   btnAdd.addEventListener("click", addInput);
-// }
+if (menuToggle) {
+  menuToggle.addEventListener("click", menuToggleClickHandler);
+}
+
